a4a1c631d33de73517f3396f778045ab754fcc16
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2026-02-05 18:24:21 +0800

    [fix] contacts section

--- src/partials/home/Contact.svelte
+++ src/partials/home/Contact.svelte
@@ -7,14 +7,62 @@
     return month >= 10 ? year + 1 : year;
   }
 
-  const year = getAcademicYear();
+  const year: number = getAcademicYear();
+  const contacts: {
+    title: string;
+    link: string;
+    img: string;
+    content: string;
+  }[] = [
+    {
+      title: "Location",
+      link: "https://maps.app.goo.gl/D5JcyKYwyHBXSkWc6",
+      img: "/icons/contact/location.svg",
+      content: "Jl. Assyafi'iyah No.247, RT.3/RW.5, Cilangkap, Kec. Cipayung, Jakarta Timur, DKI Jakarta. 13870"
+    },
+    {
+      title: "Email",
+      link: "mailto:jayakartahalalcenter@gmail.com",
+      img: "/icons/contact/mail.svg",
+      content: "jayakartahalalcenter@gmail.com"
+    },
+    {
+      title: "WhatsApp",
+      link: "https://wa.me/6282146021999",
+      img: "/icons/contact/whatsapp.svg",
+      content: "(+62) 813-2248-2220"
+    }
+  ]
 </script>
 
-<section class="w-full h-auto bg-jhc-2 text-white py-28" id="kontak">
-  <div class="container max-w-6xl mx-auto flex flex-col gap-6 justify-center items-center">
+<section class="w-full h-auto bg-jhc-2 text-white py-28 relative overflow-hidden" id="kontak">
+  <div class="absolute w-full h-full mx-auto flex justify-center items-center -top-10">
+    <img
+      src="/icons/mosaic-jhc-2.svg"
+      alt="Mosaic Light"
+      class="w-150 h-auto"
+    />
+  </div>
+  <div class="container max-w-6xl mx-auto flex flex-col gap-6 justify-center items-center relative z-10">
     <h3 class="text-5xl text-center leading-14 font-bold">Siap Menuju <span class="text-jhc-orange">Sertifikasi</span> <br/> <span class="text-jhc-orange">Halal {year}?</span></h3>
     <p class="font-light text-lg w-9/12 text-center">Amankan izin edar dan kepatuhan halal produk Anda sebelum batas wajib 2026. Tim ahli kami siap mendampingi Anda sekarang</p>
-    <div class="flex flex-row gap-4 font-medium mt-9">
+    <div class="flex flex-col gap-1 w-6/12 mx-auto px-auto py-6">
+      {#each contacts as con}
+        <a
+          href={con.link}
+          target="_blank"
+          class="flex flex-row gap-2 items-center hover:underline hover:text-jhc-light h-12"
+        >
+          <img
+            src={con.img}
+            alt={con.title}
+            class="h-10 w-auto"
+          />
+          <p class="grow">{con.content}</p>
+        </a>
+      {/each}
+    </div>
+    <div class="flex flex-row gap-4 font-medium">
       <a href="https://wa.me/6281322482220" target="_blank" class="bg-jhc-orange text-jhc py-3 px-7 rounded-full cursor-pointer hover:bg-jhc-orange/90 shadow-2xl border border-jhc-orange">
         Daftar Sertifikasi Sekarang
       </a>
