fbd3bb1c002be10955101e63972ec33eb1bcfa5b
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2026-02-05 10:15:58 +0800

    [fix] use image for under construction

--- src/partials/UnderConstruction.svelte
+++ src/partials/UnderConstruction.svelte
@@ -2,18 +2,13 @@
 	class="bg-white text-gray-800 top-0 bottom-0 left-0 right-0 h-screen w-screen overflow-x-hidden"
 >
 	<div class="w-full h-full flex flex-col">
-		<main class="flex flex-col left-0 right-0 h-full mx-5">
-			<div class="flex flex-col gap-5 justify-center items-center h-full w-full">
-				<img src="/icons/logo-mono-dark.svg" alt="JHC" class="w-full md:w-96 h-auto" />
-				<div class="flex flex-col gap-2 justify-center items-center text-center">
-					<h1 class="text-jhc-2 font-bold text-3xl md:text-4xl">Under Construction</h1>
-					<p class="md:text-lg text-base">
-						Jasa Sertifikasi Halal Terpercaya dari <a
-							href="https://jhc.or.id"
-							class="italic underline">Jayakarta Halal Center</a
-						>
-					</p>
-				</div>
+		<main class="flex flex-col left-0 right-0 h-full">
+			<div class="flex justify-center items-center max-h-full">
+				<img
+					src="/img/uc.png"
+					alt="Under Construction"
+					class="h-full"
+				/>
 			</div>
 		</main>
 	</div>
