c864b1952399bd73cc954cfaca4517a661fc47a7
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2026-02-06 02:12:38 +0800

    [fix] Content Security Policy

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,17 +1,8 @@
 <script lang="ts">
 	import Navbar from '@/partials/Navbar.svelte';
 	import Footer from '@/partials/Footer.svelte';
-	import { onMount } from 'svelte';
 	
 	let { children } = $props();
-
-	onMount(() => {
-		new PerformanceObserver((entryList) => {
-			for (const entry of entryList.getEntries()) {
-				console.log('Layout shift:', entry);
-			}
-		}).observe({type: 'layout-shift', buffered: true});
-	})
 </script>
 
 <div class="bg-white text-jhc-dark top-0 mt-20 bottom-0 left-0 right-0 overflow-x-hidden">
