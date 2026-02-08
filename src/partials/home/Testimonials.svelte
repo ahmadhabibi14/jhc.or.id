dc625b41a2de696de9fe2c3bd899f737e9507995
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2026-02-08 17:13:10 +0700

    [fix] testimonials to auto-sliding, trustedby content

--- src/partials/home/Testimonials.svelte
+++ src/partials/home/Testimonials.svelte
@@ -1,28 +1,37 @@
 <script lang="ts">
-  const testimonials: {
-  content: string;
-  name: string;
-  role: string;
-}[] = [
-  {
-    content:
-      "Pendampingan sertifikasi halal untuk pabrik parfum kami berjalan cepat, rapi, dan jelas. Kini produk kami lebih siap bersaing di pasar nasional maupun ekspor.",
-    name: "MICHAEL TAN",
-    role: "Owner Pabrik Parfum, Pulo Gadung, Jakarta Timur"
-  },
-  {
-    content:
-      "Awalnya kami merasa pengurusan sertifikat halal itu rumit dan memakan waktu. Namun setelah didampingi, semua jadi lebih mudah dan jelas. Prosesnya cepat, komunikatif, dan sangat membantu kami sebagai pelaku usaha katering.",
-    name: "RINDITYA",
-    role: "Owner Katering Bandung, Jawa Barat"
-  },
-  {
-    content:
-      "Dengan sertifikat halal, kami merasa lebih tenang karena seluruh proses pengolahan makanan di restoran kami sudah sesuai regulasi halal pemerintah. Pendampingannya profesional dan mudah dipahami.",
-    name: "JIHAD",
-    role: "Owner Restoran Timur Tengah, Depok, Jawa Barat"
-  }
-];
+  import { onMount } from "svelte";
+
+  let testimonials: {
+    content: string;
+    name: string;
+    role: string;
+  }[] = [
+    {
+      content:
+        "Pendampingan sertifikasi halal untuk pabrik parfum kami berjalan cepat, rapi, dan jelas. Kini produk kami lebih siap bersaing di pasar nasional maupun ekspor.",
+      name: "MICHAEL TAN",
+      role: "Owner Pabrik Parfum, Pulo Gadung, Jakarta Timur"
+    },
+    {
+      content:
+        "Awalnya kami merasa pengurusan sertifikat halal itu rumit dan memakan waktu. Namun setelah didampingi, semua jadi lebih mudah dan jelas. Prosesnya cepat, komunikatif, dan sangat membantu kami sebagai pelaku usaha katering.",
+      name: "RINDITYA",
+      role: "Owner Katering Bandung, Jawa Barat"
+    },
+    {
+      content:
+        "Dengan sertifikat halal, kami merasa lebih tenang karena seluruh proses pengolahan makanan di restoran kami sudah sesuai regulasi halal pemerintah. Pendampingannya profesional dan mudah dipahami.",
+      name: "JIHAD",
+      role: "Owner Restoran Timur Tengah, Depok, Jawa Barat"
+    }
+  ];
+
+  onMount(() => {
+    const original = [...testimonials];
+    while (testimonials.length < 12) {
+      testimonials = [...testimonials, ...original.slice(0, 12 - testimonials.length)];
+    }
+  });
 </script>
 
 <section class="container max-w-6xl mx-auto my-16 md:px-5 lg:px-0 px-5" id="testimonials">
@@ -31,10 +40,30 @@
       <span class="text-jhc-orange text-xl font-medium">Testimonial</span>
       <h3 class="text-jhc font-semibold text-4xl">Apa Kata Mereka ?</h3>
     </div>
-    <div class="grid md:grid-cols-3 gap-4">
-      {#each testimonials as t}
-        <div class="flex flex-col gap-2 shadow-md rounded-xl border border-gray-200 py-4 px-5 h-auto justify-between">
-          <blockquote class="text-lg italic font-medium leading-6 pb-2">
+    <div class="flex justify-center items-center">
+      <div class="w-full md:w-8/12 flex flex-col gap-2 shadow-md rounded-xl border border-gray-200 py-4 px-5 h-auto justify-between">
+        <blockquote class="text-lg italic font-medium leading-6 pb-2">
+          LPH Hidayatullah mengapresiasi kerja sama dengan Jayakarta Halal Center yang selama ini menunjukkan profesionalisme dan komitmen dalam mendukung layanan sertifikasi halal. Semoga sinergi ini terus berlanjut untuk memperkuat ekosistem halal yang terpercaya.
+        </blockquote>
+        <div class="flex flex-col gap-4">
+          <div class="w-full h-1 border-dashed border-t border-jhc-orange"></div>
+          <div class="flex flex-col justify-end gap-0 items-end">
+            <span class="text-sm font-semibold">Muhammad Faisal</span>
+            <p class="text-xs text-end">Ketua LPH Hidayatullah</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="testimonial-wrapper h-[270px]">
+      {#each testimonials as t, idx}
+        <div
+          style="animation-delay: calc(50s / 6 * (6 - {idx+1}) * -1);"
+          class="testimonial-item h-[260px]
+          flex flex-col gap-2 shadow-md rounded-xl border border-gray-200
+          py-4 px-5
+          justify-between"
+        >
+          <blockquote class="italic font-medium leading-6 pb-2">
             {t.content}
           </blockquote>
           <div class="flex flex-col gap-4">
@@ -49,3 +78,44 @@
     </div>
   </div>
 </section>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+
+  .testimonial-wrapper {
+    width: 90%;
+    max-width: 1536px;
+    margin-inline: auto;
+    position: relative;
+    overflow: hidden;
+    mask-image: linear-gradient(
+      to right,
+      rgba(0, 0, 0, 0),
+      rgba(0, 0, 0, 1) 20%,
+      rgba(0, 0, 0, 1) 80%,
+      rgba(0, 0, 0, 0)
+    );
+  }
+
+  @keyframes scrollLeftTestimoni {
+    to {
+      left: -400px;
+    }
+  }
+
+  @keyframes scrollLeft2Testimoni {
+    to {
+      left: -480px;
+    }
+  }
+
+  .testimonial-item {
+    width: 400px;
+    position: absolute;
+    left: max(calc(360px * 6), 100%);
+    animation-name: scrollLeftTestimoni;
+    animation-duration: 50s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+  }
+</style>
--- src/partials/home/TrustedBy.svelte
+++ src/partials/home/TrustedBy.svelte
@@ -1,6 +1,16 @@
-<script>
+<script lang="ts">
 	import { Icon } from "svelte-icons-pack";
 	import { RiSystemCheckboxCircleFill } from "svelte-icons-pack/ri";
+
+  const features: string[] = [
+    'UMKM industri pengolahan makanan dan minuman',
+    'Satuan Pelayanan Pemenuhan Gizi (SPPG) MBG',
+    'Restoran dan cafe',
+    'Katering',
+    'Pabrik kosmetik',
+    'Jasa Penyembelihan Rumah Potong Hewan',
+    'Dan lain sebagainya'
+  ]
 </script>
 
 <section class="container max-w-6xl mx-auto my-16 md:px-0 px-5" id="dipercaya">
@@ -10,33 +20,24 @@
       <h3 class="text-jhc font-semibold text-4xl">Dipercaya Pelaku Usaha, Diakui Negara.</h3>
     </div>
     <div class="md:grid md:grid-cols-2 flex flex-col gap-5">
-      <div class="flex flex-col gap-7">
-        <p>Bergabunglah dengan jaringan pengusaha yang telah sukses naik kelas. Kami mengawal ketat standarisasi halal untuk memastikan brand Anda memiliki keunggulan kompetitif di pasar dengan label Halal resmi.</p>
+      <div class="flex flex-col gap-4">
+        <p>
+          Bergabunglah dengan jaringan pengusaha yang telah sukses naik kelas. Kami mengawal mulai dari tahap persiapan hingga evaluasi Penerapan Sistem Jaminan Produk Halal untuk memastikan brand Anda memiliki keunggulan kompetitif di pasar dengan label Halal resmi BPJPH.  
+        </p>
+        <p>
+          Telah membantu Fasilitasi Pengurusan Sertifikat Halal:
+        </p>
         <div class="flex flex-col gap-1 text-jhc-2">
-          <div class="flex flex-row gap-2 items-center">
-            <Icon
-              src={RiSystemCheckboxCircleFill}
-              size="18"
-              className="text-jhc-orange fill-jhc-orange"
-            />
-            <p>Spesialis Pendampingan Unit Gizi & MBG</p>
-          </div>
-          <div class="flex flex-row gap-2 items-center">
-            <Icon
-              src={RiSystemCheckboxCircleFill}
-              size="18"
-              className="text-jhc-orange fill-jhc-orange"
-            />
-            <p>Solusi Sertifikasi Lintas Industri</p>
-          </div>
-          <div class="flex flex-row gap-2 items-center">
-            <Icon
-              src={RiSystemCheckboxCircleFill}
-              size="18"
-              className="text-jhc-orange fill-jhc-orange"
-            />
-            <p>Jaminan Kriteria BPJPH</p>
-          </div>
+          {#each features as feature}
+            <div class="flex flex-row gap-2 items-center">
+              <Icon
+                src={RiSystemCheckboxCircleFill}
+                size="18"
+                className="text-jhc-orange fill-jhc-orange"
+              />
+              <p>{feature}</p>
+            </div>
+          {/each}
         </div>
       </div>
       <div class="bpjhp-container md:rounded-2xl flex justify-center items-center md:h-[200px] lg:h-[300px] w-full h-auto">
